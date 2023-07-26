531eedd747b58949d924aca3a2e4f9f464dde692
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-26 10:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.get('/', (req, res) => {
-	res.send("Hello Friends...");
+	res.send("Hello Friends...!");
 })
 
 app.listen(PORT, () => console.log("App listening at port " + PORT));
